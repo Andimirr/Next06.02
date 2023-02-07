114ad27ba32fa943c236f16927e190255ebb9db8
Andimirr <andimirr@gmail.com>
2023-02-07 20:52:58 +0300

    квадрат числа

---
+++ 001_квадрат/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает
+// число и выдает его квадрат (число умноженное
+// на само себя).
+// Например:
+// ● 4 -> 16
+// ● -3 -> 9
+// ● -7 -> 49
+
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b ");
+int b = int.Parse(Console.ReadLine()!);
+if (b * b == a)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
